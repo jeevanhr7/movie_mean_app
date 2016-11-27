63a858ad19ca5edb63b728d135f702b2f1df4300
Jeevan HR <jeevanhr7@users.noreply.github.com>
2016-11-27 23:29:33 +0530

    Update production.js

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -1,13 +1,14 @@
-//module.exports = {
-//    ip: process.env.IP || undefined,
-//    port: process.env.PORT || 3000,
-//    mongo: 'mongodb://qnotify:quezx123@ds063439.mongolab.com:63439/tsc'
-//
-//}
+module.exports = {
+    ip: process.env.IP || undefined,
+    port: process.env.PORT || 3000,
+    mongo: 'mongodb://jeevan:jeevanhr1.@ds111748.mlab.com:11748/jeevan'
 
+}
+
+/*
 module.exports = {
     ip: "localhost",
     port: 3000,
     mongo:"mongodb://localhost/MovieSchema"
 
-}
+}*/
